d1e8c7a74015a0c8e26eb4199fbab229035b9d38
Randy Do <randyydoo@csu.fullerton.edu>
2023-02-23 13:15:57 -0800

    new

---
+++ vanessa.py
@@ -0,0 +1,59 @@
+#create a fucntiom that take an positive integer "n" and print the sum of 1 - n using while loop
+"""
+>>whilee(6) = 1 + 2 + 3 + 4 + 5 + 6
+21
+>>whilee(3) = 1, 2, 3
+6
+>>whilee(-1)
+False
+"""
+def while_(n):
+    
+    
+    
+    
+    return
+
+#create a function that checks how many times a number appears in an integer
+"""
+checker(12234, 2)
+2
+checker(3333111, 3)
+4
+checker(1234, 5)
+0
+"""
+def checker(num , target):
+    
+    
+    
+    
+    return
+
+#crate a fucntion that removes all duplicate numbers
+""""
+>>dup(1223344)
+1234
+>>dup(55432)
+5432
+>>dup(6789)
+6789
+"""
+def dup(n):
+    
+    
+    
+    
+    return n
+#create a palindrome checker for a string
+"""
+>>palindrome("hello")
+False
+>>palindrome("mom")
+true
+"""
+def palindrome(s):
+    
+    
+    
+    return True
