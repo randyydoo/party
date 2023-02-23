7ccbfb671ccf17be56bb3b1ce059d15a5496693b
Randy Do <randyydoo@csu.fullerton.edu>
2023-02-22 22:22:47 -0800

    added all for 2/23

---
+++ randy.py
@@ -0,0 +1,27 @@
+#crate a fucntion that removes all duplicate numbers
+""""
+>>dup(1223344)
+1234
+>>dup(55432)
+5432
+>>dup(6789)
+6789
+"""
+def dup(n):
+    
+    
+    
+    
+    return n
+#create a palindrome checker for a string
+"""
+>>palindrome("hello")
+False
+>>palindrome("mom")
+true
+"""
+def palindrome(s):
+    
+    
+    
+    return True
