d5567480f94eda45f338fe75c3e43cad0dc98c46
NateGries1 <98416636+NateGries1@users.noreply.github.com>
2023-02-26 00:08:30 -0800

    Update nate.py
    Signed-off-by: NateGries1 <98416636+NateGries1@users.noreply.github.com>

--- nate.py
+++ nate.py
@@ -7,12 +7,17 @@
 >>dup(6789)
 6789
 """
+
+23456.7
+23456
 def dup(n):
-    
-    
-    
-    
-    return n
+    list = ""
+    for char in str(n):
+        if list.find(char) != -1:
+            list += char
+    return int(list)
+
+
 #create a palindrome checker for a string
 """
 >>palindrome("hello")
@@ -20,8 +25,15 @@ False
 >>palindrome("mom")
 true
 """
-def palindrome(s):
-    
-    
-    
-    return True
+def palindrome(n):
+    return recurse(n, 0, len(n))
+def recurse(s, first, last):
+    if first >= last - 1:
+        return True
+    if s[first] == s[last - 1]:
+        return palindrome(s, first + 1, last - 1)
+    return False
+
+print(palindrome("racecar", 0, 7))
+print(palindrome("mom", 0, 3))
+print(palindrome("hello", 0, 5))
